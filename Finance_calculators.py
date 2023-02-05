d1b7dd14c08467b269654988ec4e9b788a09a9ed
Thomasbamber99 <Thomasbamber99@gmail.com>
2023-02-05 21:49:00 +0000

    Refactoring with use of functions

--- Finance_calculators.py
+++ Finance_calculators.py
@@ -1,64 +1,66 @@
 import math 
-# First choice that the user faces, I  use the .upper function for the first input, this is to ensure the computer recognises what the user enters
+
+
+def profit(x, y):
+    return(round(x - y, 2))
+
+def simple(x, y, z):
+    y = y / 100
+    return(round(x *(1 + y * z),2))
+
+def compound(x, y, z):
+    y = y / 100
+    return(round(x *(1 + y ** z),2))
+
+def bond_payments(x, y, z):
+    y = (y / 100) / 12
+    return((x * y) / (1 - (1 + y) ** (- z)))
+
+
 print ("investment - Calculates the amount of interest you will earn on your investment portfolio.\n")
 print ("bond - Calculates the amount you will have to pay every month for a home loan\n")
-choice = input("Choose either 'Investment' or 'Bond' from the menu below to proceed: ").upper()
-choice1 = "INVESTMENT"
-choice2 = "BOND"
 
-# Conditional checks for inputs from the user
-if choice == "INVESTMENT":
+answer = input("Choose either 'Investment' or 'Bond' from the menu below to proceed: ").upper()
+
+if answer == "INVESTMENT":
     deposit = float(input("Please enter the amount that you are depositing: "))
+    interest = float(input("Please enter the interest rate. eg 8: "))
+    plan = int(input("How many years do you plan on investing? "))
     
-    # Checking whether the user is sure about the amount stored in the input
-    check1 =  input((f"You wish to deposit £{deposit}, is this correct? [Yes/No] ")).capitalize()
-    if check1 == "No":
-        deposit = float(input("Please enter the amount that you are depositing: "))
-        
-        # Below is if user selects that they are happy with their choice.
-    elif    check1 == "Yes":
-            user_perc = float(input("Please enter the interest rate. eg 8: "))
-            percentage = user_perc / 100
-            plan = int(input("How many years do you plan on investing? "))
-            interest = input("Would you like to know your simple or compound interest? ").capitalize()
-            
-            # Formula to work Simple interest
-            simple_int = (round(deposit*(1 + percentage * plan),2))
-            profit1 = (round(simple_int - deposit,2))
-            
-            # Formula to work out Compound interest
-            comp_int = (round(deposit*((1+ percentage)** plan),2))
-            profit2 = (round(comp_int - deposit,2))
-            
-            if interest == "Simple":
-                print (f"Your money will increase to the figure of: £{simple_int} due to simple interest rates. (The total profit is £{profit1})")
-            elif interest == "Compound":
-                print (f"Your money will have increased to the figure of: £{comp_int} thanks to compound interest. (The total profit is £{profit2})")
-                # This next line of code is just incase the user does not enter the right information so the program will run it once again.
-            else:
-                print ("I'm sorry, I didn't recognise that please try again.\n")
-                interest = input("Would you like to know your simple or compound interest? ").capitalize()
-                if interest == "Simple":
-                    print (f"Your money will increase to the figure of: £{simple_int} due to simple interest rates. (The total profit is £{profit1})")
-                elif interest == "Compound":
-                    print (f"Your money will have increased to the figure of: £{comp_int} thanks to compound interest. (The total profit is £{profit2}) ")
-                
-elif choice == "BOND":
-    # User inputs
+    interest = input("Would you like to know your simple or compound interest? ").capitalize()
+    
+
+    if interest == "Simple":
+        result = simple(deposit, interest, plan)
+        profit(result, deposit )
+
+        print(f"Your money will increase to the figure of: £{result} due to simple interest rates. (The total profit is £{profit})")
+    
+
+    elif interest == "Compound":
+        result = compound(deposit, interest, plan)
+        profit(result, deposit )
+
+        print (f"Your money will have increased to the figure of: £{result} thanks to compound interest. (The total profit is £{profit})")
+    
+
+    else:
+        print ("I'm sorry, I didn't recognise that please try again.\n")
+
+
+elif answer == "BOND":
+
     house_value = float(input("What is the value of your house?: "))
     house_interest = int(input("Please enter the interest rate of the house eg.8: "))
     months = int(input("How many months do you plan on taking to repay the bond? "))
-   
-    # Forumla for working out repayments using user inputs
-    monthly_interest = (house_interest / 100) / 12
-    bond_formula = ((house_value * monthly_interest)/(1 -(1 + monthly_interest)**(- months)))
-    answer = (round(bond_formula, 2))
-    
-    print (f"The amount you will pay monthly is: £{answer}")
+
+    result = round(bond_payments(house_value, house_interest, months),2)
+
+    print(f"Your monthly repayments will amount to: £{result}")
+
+
 else:
     print ("I didn't understand that, could you please try again? :)")
-
-# Error message for user to let them know they haven't input the right information :)
   
 
 
